419e82a61b8feb04e4ebdbbb97141c38f9b57c16
Luiz Caldeirao <claudiocaldeirao@gmail.com>
2025-05-13 00:57:08 -0300

    feat: add Swagger documentation and versioning to the E-Commerce API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce API')
+    .setDescription(
+      'This is the API documentation for the E-Commerce application.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
